7c4beb4bf73c3671cba04f54885b99c472e6681e
JohnMata0427 <john.mata@epn.edu.ec>
2024-07-17 19:29:57 -0500

    Update nodemailer configuration and email templates

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,47 +1,51 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
+import 'dotenv/config'
 
 let transporter = nodemailer.createTransport({
-    service:'gmail',
-    host:process.env.HOST_MAILTRAP,
-    port:process.env.PORT_MAILTRAP,
-    auth:{
-        user:process.env.USER_MAILTRAP,
-        pass:process.env.PASS_MAILTRAP
-    }
+    service: "gmail",
+    host: process.env.HOST_MAILTRAP,
+    port: process.env.PORT_MAILTRAP,
+    auth: {
+        user: process.env.USER_MAILTRAP,
+        pass: process.env.PASS_MAILTRAP,
+    },
 });
 
-const sendMailToUser = ( userMail , token ) => {
+const sendMailToUser = (userMail, token) => {
     let mailOptions = {
         from: process.env.USER_MAILTRAP,
         to: userMail,
-        subject: 'Verifica tu cuenta',
-        html: `<p>Hola, haz clic <a href="${process.env.URL_BACKEND}confirmar/${encodeURIComponent(token)}">aquí</a> para confirmar tu cuenta.</p>`
+        subject: "Verifica tu cuenta - ESFOT Association Food Order 🍛🍽️",
+        html: `<p>Hola, haz clic <a href="${
+            process.env.URL_BACKEND
+        }confirmar/${encodeURIComponent(
+            token
+        )}">aquí</a> para confirmar tu cuenta.</p>`,
     };
-    transporter.sendMail(mailOptions , function ( error , info ){
-        if (error) {console.log(error);}
-        else {console.log('Correo enviado: ' + info.response);}
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Correo enviado: " + info.response);
+        }
     });
-}
+};
 
-const sendMailToRecoveryPassword = async ( userMail , token ) => {
+const sendMailToRecoveryPassword = async (userMail, token) => {
     let info = await transporter.sendMail({
-    from: 'admin@vet.com',
-    to: userMail,
-    subject: "Correo para reestablecer tu contraseña",
-    html: `
+        from: "admin@vet.com",
+        to: userMail,
+        subject:
+            "Correo para reestablecer tu contraseña - ESFOT Association Food Order 🍛🍽️",
+        html: `
     <h1>Sistema de gestión (FOOD - ESFOT)</h1>
     <hr>
     <a href=${process.env.URL_BACKEND}chef/recuperarpassword/${token}>Clic para reestablecer tu contraseña</a>
     <hr>
     <footer>Bienvenido!</footer>
-    `
+    `,
     });
     console.log("Mensaje enviado satisfactoriamente: ", info.messageId);
-}
+};
 
-export {
-    sendMailToRecoveryPassword,
-    sendMailToUser
-}
+export { sendMailToRecoveryPassword, sendMailToUser };
